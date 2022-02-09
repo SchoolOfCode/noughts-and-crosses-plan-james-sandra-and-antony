f13c0776bb787df2d990beaf28ea90cd1f30e19a
jamesgaynor <james_gaynor@btinternet.com>
2022-02-09 16:53:41 +0000

    working game

--- game-project/src/components/Square/Square.js
+++ game-project/src/components/Square/Square.js
@@ -1,8 +1,18 @@
 import css from "./Square.module.css";
-
+import { useState } from "react";
 export default function Square({ choice, handleClick, value }) {
+  const [isDisabled, setisDisabled] = useState(false);
+
   return (
-    <button className={css.buttons} onClick={handleClick} value={value}>
+    <button
+      className={css.buttons}
+      disabled={isDisabled}
+      onClick={() => {
+        setisDisabled(true);
+        handleClick();
+      }}
+      value={value}
+    >
       {choice}
     </button>
   );
